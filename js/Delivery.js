1e4e7f308b3b3b9c39132735fb854a3c6e89f667
Stanislav Shastov <shastov.stas1991@gmail.com>
2024-11-11 17:02:48 +0300

    create module Delivery

---
+++ js/Delivery.js
@@ -0,0 +1,25 @@
+export default class Delivery {
+    constructor(name, adress, distance) {
+        this.name = name;
+        this.adress = adress;
+        this.distance = distance;
+    }
+
+    getCard() {
+        this.sectionElement = document.querySelector('.main-section'); 
+        this.divElement = document.createElement('div');
+        this.divElement.classList.add('card');
+        this.sectionElement.append(this.divElement);
+        
+        return this.divElement;
+    }
+
+    createTitle(titleName = 'Имя:', titleAdress = 'Адрес', titleDistance = 'Расстояние') {
+        this.getCard = document.querySelector('.card');
+        this.createTitleDiv = document.createElement('div');
+        this.createTitleDiv.classList.add('card__title');
+
+
+
+    }
+}
--- script.js
+++ script.js
@@ -1,33 +1,3 @@
-class Delivery {
-    constructor(name, adress, distance) {
-        this.name = name;
-        this.adress = adress;
-        this.distance = distance;
-    }
-
-    getCard() {
-        this.sectionElement = document.querySelector('.main-section'); 
-        this.divElement = document.createElement('div');
-        this.divElement.classList.add('card');
-        this.sectionElement.append(this.divElement);
-        
-        return this.divElement;
-    }
-
-    createTitle(titleName = 'Имя:', titleAdress = 'Адрес', titleDistance = 'Расстояние') {
-        this.getCard = document.querySelector('.card');
-        this.createTitleDiv = document.createElement('div');
-        this.createTitleDiv.classList.add('card__title');
-
-        // const 
-        // getCard.append(createTitleDiv);
-
-        this.tagP = document.createElement('p');
-        this.tagP.textContent = this.name;
-        this.createTitleDiv.append(this.tagP);
-    }
-}
-
 const card1 = new Delivery('Ольга', 'ул. Вымыслов, д.12', 8);
 card1.getCard();
 console.log(card1);
