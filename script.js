90cd0b7ac8c20660ba1b8fbcae9c3131d6de8de8
Stanislav Shastov <shastov.stas1991@gmail.com>
2024-11-11 16:21:20 +0300

    created three cards

--- script.js
+++ script.js
@@ -16,4 +16,10 @@ class Delivery {
 }
 
 const card1 = new Delivery('Ольга', 'ул. Вымыслов, д.12', 8);
-console.log(card1.getCard());
+card1.getCard();
+
+const card2 = new Delivery('Ольга', 'ул. Вымыслов, д.12', 8);
+card2.getCard();
+
+const card3 = new Delivery('Ольга', 'ул. Вымыслов, д.12', 8);
+card3.getCard();
