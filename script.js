735718e3d0759466366555e9a945ce5bec982443
Stanislav Shastov <shastov.stas1991@gmail.com>
2024-11-11 16:57:52 +0300

    this,this,this,this

--- script.js
+++ script.js
@@ -6,28 +6,33 @@ class Delivery {
     }
 
     getCard() {
-        const sectionElement = document.querySelector('.main-section'); 
-        const divElement = document.createElement('div');
-        divElement.classList.add('card');
-        sectionElement.append(divElement);
+        this.sectionElement = document.querySelector('.main-section'); 
+        this.divElement = document.createElement('div');
+        this.divElement.classList.add('card');
+        this.sectionElement.append(this.divElement);
         
-        return divElement;
+        return this.divElement;
     }
 
-    createTitle(titleName, titleAdress, titleDistance) {
-        const getCard = document.querySelector('.card');
-        const createTitleDiv = document.createElement('div');
-        getCard.append(createTitleDiv);
+    createTitle(titleName = 'Имя:', titleAdress = 'Адрес', titleDistance = 'Расстояние') {
+        this.getCard = document.querySelector('.card');
+        this.createTitleDiv = document.createElement('div');
+        this.createTitleDiv.classList.add('card__title');
 
-        const tagP = document.createElement('p');
-        tagP.textContent = this.name;
-        createTitleDiv.append(tagP);
+        // const 
+        // getCard.append(createTitleDiv);
+
+        this.tagP = document.createElement('p');
+        this.tagP.textContent = this.name;
+        this.createTitleDiv.append(this.tagP);
     }
 }
 
 const card1 = new Delivery('Ольга', 'ул. Вымыслов, д.12', 8);
-card1.createTitle('Имя:', 'Адрес:', 'Расстояние:');
 card1.getCard();
+console.log(card1);
+card1.createTitle();
+
 
 const card2 = new Delivery('Ольга', 'ул. Вымыслов, д.12', 8);
 card2.getCard();
