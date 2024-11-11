1fcd8e9c8a3c421b1764ba2d766d40e67cf38807
Stanislav Shastov <shastov.stas1991@gmail.com>
2024-11-11 16:00:15 +0300

    initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+class Delivery {
+    constructor(name, adress, distance) {
+        this.name = name;
+        this.adress = adress;
+        this.distance = distance;
+    }
+
+    getCard {
+        const 
+    }
+}
+
+const card1 = 
