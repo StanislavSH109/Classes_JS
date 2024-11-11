b24fa35ea884386cad6fd7fd3be2279ac2126144
Stanislav Shastov <shastov.stas1991@gmail.com>
2024-11-11 16:16:47 +0300

    created new class and added styles in card, normalize

--- script.js
+++ script.js
@@ -5,9 +5,15 @@ class Delivery {
         this.distance = distance;
     }
 
-    getCard {
-        const 
+    getCard() {
+        const sectionElement = document.querySelector('.main-section'); 
+        const divElement = document.createElement('div');
+        divElement.classList.add('card');
+        sectionElement.append(divElement);
+        
+        return divElement;
     }
 }
 
-const card1 = 
+const card1 = new Delivery('Ольга', 'ул. Вымыслов, д.12', 8);
+console.log(card1.getCard());
