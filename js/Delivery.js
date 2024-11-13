799cc8f53c5048321e0623c5d9b649ff967450e3
Stanislav Shastov <shastov.stas1991@gmail.com>
2024-11-13 20:03:05 +0300

    change method from class

--- js/Delivery.js
+++ js/Delivery.js
@@ -10,9 +10,9 @@ export default class Delivery {
         this.divElement = document.createElement('div');
         this.divElement.classList.add('card');
         
-        this.addTitleName = this.createTitle('Имя:', this.name);
-        this.addTitleAdress = this.createTitle('Адрес:', this.adress);
-        this.addTitleDistance = this.createTitle('Расстояние:', this.distance);
+        this.addTitleName = this.createTitle('Имя:')
+        this.addTitleAdress = this.createTitle('Адрес:');
+        this.addTitleDistance = this.createTitle('Расстояние:');
         
         this.sectionElement.append(this.divElement);
         this.divElement.append(this.addTitleName, this.addTitleAdress, this.addTitleDistance);
@@ -20,16 +20,30 @@ export default class Delivery {
         return this.divElement;
     }
 
-    createTitle(titleName, value) {
+    createTitle(titleName) {
         this.getCard = document.querySelector('.card');
         this.createTitleDiv = document.createElement('div');
         this.createTitleDiv.classList.add('card__title');
         this.createTitleH2 = document.createElement('h2');
         this.createTitleH2.textContent = titleName;
-        this.createTitleText = document.createElement('p');
-        this.createTitleText.textContent = value;
-        this.createTitleDiv.append(this.createTitleH2, this.createTitleText);
+
+        this.createTitleDiv.append(this.createTitleH2);
 
         return this.createTitleDiv;
+    }
+
+    // set titleName(name) {
+    //     this.addTitleName = this.createTitle('Имя:', this.name);
+    // }
+
+    set positionText(value) {
+        this.createTitleDivAll = document.querySelectorAll('.card__title');
+        this.createTitleDivAll.forEach(element => {
+            element.style.alignItems = value;
+        });
+    }
+
+    get positionText() {
+        return this.createTitleDiv.style.alignItems;
     }
 }
