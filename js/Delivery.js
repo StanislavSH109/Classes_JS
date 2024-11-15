c53b2e6d7f2fda0c02d7c4c313e3f71e836ca322
Stanislav Shastov <shastov.stas1991@gmail.com>
2024-11-15 16:40:07 +0300

    finished first TASK

--- js/Delivery.js
+++ js/Delivery.js
@@ -16,40 +16,59 @@ export default class Delivery {
         this.divElement.classList.add('card');
         
         this.nameElement = document.createElement('h2');
-        this.textElement = document.createElement('p');
+        this.nameTextElement = document.createElement('p');
         this.nameElement.textContent = 'Имя';
-        this.textElement.textContent = `${this.name}`
+        this.nameTextElement.textContent = `${this.name}`
         this.divElement.append(this.nameElement);
-        this.divElement.append(this.textElement);
+        this.divElement.append(this.nameTextElement);
        
         this.nameElement = document.createElement('h2');
-        this.textElement = document.createElement('p');
+        this.adressTextElement = document.createElement('p');
         this.nameElement.textContent = 'Адрес';
-        this.textElement.textContent = `${this.adress}`
+        this.adressTextElement.textContent = `${this.adress}`
         this.divElement.append(this.nameElement);
-        this.divElement.append(this.textElement);
+        this.divElement.append(this.adressTextElement);
        
         this.nameElement = document.createElement('h2');
-        this.textElement = document.createElement('p');
+        this.distanceTextElement = document.createElement('p');
         this.nameElement.textContent = 'Расстояние';
-        this.textElement.textContent = `${this.distance} км.`
+        this.distanceTextElement.textContent = `${this.distance} км.`
         this.divElement.append(this.nameElement);
-        this.divElement.append(this.textElement);
+        this.divElement.append(this.distanceTextElement);
 
         this.sectionElement.append(this.divElement);
         return this.divElement;
     }
 
-    // set updateName(name){
-    //     if(this.textElement){
-    //         this.cardElement = document.getElementById(`${id}`);
-    //         this.cardElement.textElement.textContent = name;
-    //     }
-    // }
+    set updateName(name) {
+        if(this.name) {
+            this.nameTextElement.textContent = name;
+        }
+    }
 
-    // get updateName() {
-    //     return this.textElement;
-    // }
+    get updateName() {
+        return this.nameTextElement;
+    }
+
+    set updateAdress(adress) {
+        if(this.adress) {
+            this.adressTextElement.textContent = adress;
+        }
+    }
+
+    get updateAdress() {
+        return this.adressTextElement;
+    }
+
+    set updateDistance(distance) {
+        if(this.distance){
+            this.distanceTextElement.textContent = `${distance} км.`;
+        }
+    }
+
+    get updateDistance() {
+        this.distanceTextElement;
+    }
 
     set align(option) {
         if(this.divElement) {
