5a2175118e9809dc78852445819527e075d8c044
Stanislav Shastov <shastov.stas1991@gmail.com>
2024-11-23 19:47:37 +0300

    created foreach with classes

--- js/script.js
+++ js/script.js
@@ -6,12 +6,6 @@ const deliveryArray = [
     new EditDelivery('Алексей', 'ул. Солнечный проезд, д. 22', 18, 'delivered')
 ];
 
-
-const card1 = deliveryArray[0];
-card1.getCard();
-
-const card2 = deliveryArray[1];
-card2.getCard();
-
-const card3 = deliveryArray[2];
-card3.getCard();
+deliveryArray.forEach(delivery => {
+    delivery.getCard();
+});
